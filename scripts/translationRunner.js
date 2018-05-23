3186db0a233f13d8a85a7cae0aeb793a41ef9f23
Mart Roosmaa <mart@roosmaa.net>
2018-05-23 16:13:32 +0100

    Migrate to riw for managing translations

--- scripts/translationRunner.js
+++
@@ -1,7 +0,0 @@
-const manageTranslations = require('react-intl-translations-manager').default;
-
-manageTranslations({
-    messagesDirectory: 'src/translations/extracted/',
-    translationsDirectory: 'src/translations/locales/',
-    languages: ['de', 'es', 'et', 'fr', 'it', 'hu', 'nl', 'pl', 'ro', 'ru', 'uk', 'zh', 'zh'],
-});
