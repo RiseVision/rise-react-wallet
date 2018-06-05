b90c954b9c9d88ff3ad180775239c46b25e5a87b
Mart Roosmaa <mart@roosmaa.net>
2018-06-05 18:10:22 +0100

    Add rise icon to onboarding titles

--- src/containers/OnboardingChooseLanguagePage.tsx
+++ src/containers/OnboardingChooseLanguagePage.tsx
@@ -8,6 +8,9 @@ import ModalPaper from '../components/ModalPaper';
 import ModalPaperHeader from '../components/ModalPaperHeader';
 import FlagIcon from '../components/FlagIcon';
 import { Locale, Locales, getUserLocales, getMainCountryForLocale } from '../utils/i18n';
+import { withStyles, WithStyles } from '@material-ui/core/styles';
+
+const riseIcon = require('../images/rise_icon.svg');
 
 // Labels are hard-coded here as we want to load the actual localization data
 // after the user has selected the language, not before.
@@ -29,60 +32,88 @@ const localeLabels: {
   zh: '繼續用中文',
 };
 
+type OnboardingChooseLanguagePageClassKey =
+  | 'titleIcon';
+
+const stylesDecorator = withStyles<OnboardingChooseLanguagePageClassKey>(
+  {
+    titleIcon: {
+      margin: '-4px 4px',
+    },
+  },
+  { name: 'OnboardingChooseLanguagePage' }
+);
+
 interface Props {
   open: boolean;
   onLanguageSelected: (locale: Locale) => void;
 }
 
-class OnboardingChooseLanguagePage extends React.Component<Props> {
-  handleLanguageClicked = (locale: Locale) => {
-    this.props.onLanguageSelected(locale);
+type DecoratedProps = Props & WithStyles<OnboardingChooseLanguagePageClassKey>;
+
+const OnboardingChooseLanguagePage = stylesDecorator<Props>(
+  class extends React.Component<DecoratedProps> {
+    handleLanguageClicked = (locale: Locale) => {
+      this.props.onLanguageSelected(locale);
+    }
+
+    render() {
+      // Order languages by browser preference
+      const userLanguages = getUserLocales();
+      let languages = Locales.map((locale) => {
+        return {
+          locale: locale,
+          countryCode: getMainCountryForLocale(locale),
+          label: localeLabels[locale],
+        };
+      });
+      languages.sort((a, b) => {
+        let aIdx = userLanguages.indexOf(a.locale);
+        let bIdx = userLanguages.indexOf(b.locale);
+
+        if (aIdx < 0) { aIdx = userLanguages.length; }
+        if (bIdx < 0) { bIdx = userLanguages.length; }
+        return aIdx - bIdx;
+      });
+
+      const { classes } = this.props;
+
+      return (
+        <ModalPaper open={this.props.open}>
+          <ModalPaperHeader>
+            <FormattedMessage
+              id="onboarding-choose-language.title"
+              description="Choose language screen title"
+              defaultMessage="{icon} RISE wallet"
+              values={{
+                icon: (
+                  <img
+                    className={classes.titleIcon}
+                    src={riseIcon}
+                    height={24}
+                    alt=""
+                  />
+                ),
+              }}
+            />
+          </ModalPaperHeader>
+          <List>
+            {languages.map(lang => (
+              <ListItem
+                key={lang.locale}
+                button={true}
+                onClick={this.handleLanguageClicked.bind(this, lang.locale)}
+              >
+                <FlagIcon countryCode={lang.countryCode} />
+                <ListItemText>{lang.label}</ListItemText>
+                <ChevronRight />
+              </ListItem>
+            ))}
+          </List>
+        </ModalPaper>
+      );
+    }
   }
-
-  render() {
-    // Order languages by browser preference
-    const userLanguages = getUserLocales();
-    let languages = Locales.map((locale) => {
-      return {
-        locale: locale,
-        countryCode: getMainCountryForLocale(locale),
-        label: localeLabels[locale],
-      };
-    });
-    languages.sort((a, b) => {
-      let aIdx = userLanguages.indexOf(a.locale);
-      let bIdx = userLanguages.indexOf(b.locale);
-
-      if (aIdx < 0) { aIdx = userLanguages.length; }
-      if (bIdx < 0) { bIdx = userLanguages.length; }
-      return aIdx - bIdx;
-    });
-
-    return (
-      <ModalPaper open={this.props.open}>
-        <ModalPaperHeader>
-          <FormattedMessage
-            id="onboarding-choose-language.title"
-            description="Choose language screen title"
-            defaultMessage="RISE wallet"
-          />
-        </ModalPaperHeader>
-        <List>
-          {languages.map(lang => (
-            <ListItem
-              key={lang.locale}
-              button={true}
-              onClick={this.handleLanguageClicked.bind(this, lang.locale)}
-            >
-              <FlagIcon countryCode={lang.countryCode} />
-              <ListItemText>{lang.label}</ListItemText>
-              <ChevronRight />
-            </ListItem>
-          ))}
-        </List>
-      </ModalPaper>
-    );
-  }
-}
+);
 
 export default OnboardingChooseLanguagePage;
