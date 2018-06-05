48a8034872caf055762eac579a5f7ad9d9d83056
Mart Roosmaa <mart@roosmaa.net>
2018-06-05 11:45:32 +0100

    Remove service worker and manifest to not distract testers

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
 
 const root = (
   <App />
 );
 
 ReactDOM.render(root, document.getElementById('root') as HTMLElement);
-registerServiceWorker();
