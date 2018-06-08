1df82fb07c1857550314e6c23147f591c325ec7e
Mart Roosmaa <mart@roosmaa.net>
2018-06-08 12:10:04 +0100

    Add basic responsive drawer layout

--- src/containers/Wallet.tsx
+++ src/containers/Wallet.tsx
@@ -1,12 +1,145 @@
 import * as React from 'react';
+import { InjectedIntlProps, injectIntl, defineMessages } from 'react-intl';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Hidden from '@material-ui/core/Hidden';
+import Drawer from '@material-ui/core/Drawer';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Theme, createStyles, withStyles, WithStyles, withTheme, WithTheme } from '@material-ui/core/styles';
 
-interface Props {
+const drawerWidth = 240;
+
+const styles = (theme: Theme) => createStyles({
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  drawerIcon: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+interface Props extends WithStyles<typeof styles> {
 }
 
-class Wallet extends React.Component<Props> {
-  render() {
-    return null;
+interface State {
+  mobileDrawerOpen: boolean;
+}
+
+type DecoratedProps = Props & InjectedIntlProps & WithTheme;
+
+const stylesDecorator = withStyles(styles, { 'name': 'Wallet' });
+
+const messages = defineMessages({
+  openDrawerAriaLabel: {
+    id: 'wallet.open-drawer-aria-label',
+    description: 'Accessibility label for open drawer icon',
+    defaultMessage: 'Open drawer',
+  },
+});
+
+const Wallet = withTheme()(stylesDecorator(injectIntl(
+  class extends React.Component<DecoratedProps, State> {
+    constructor(props: DecoratedProps) {
+      super(props);
+      this.state = {
+        mobileDrawerOpen: false,
+      };
+    }
+
+    render() {
+      const { intl, classes, theme } = this.props;
+
+      const drawer = (
+        <span>Drawer content TODO</span>
+      );
+
+      return (
+        <div className={classes.root}>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                className={classes.drawerIcon}
+                aria-label={intl.formatMessage(
+                  messages.openDrawerAriaLabel)}
+                color="inherit"
+                onClick={this.handleDrawerToggle}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" noWrap={true}>
+                Title TODO
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Hidden mdUp={true}>
+            <Drawer
+              variant="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileDrawerOpen}
+              onClose={this.handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+              children={drawer}
+            />
+          </Hidden>
+          <Hidden smDown={true} implementation="css">
+            <Drawer
+              variant="permanent"
+              open={true}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              children={drawer}
+            />
+          </Hidden>
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Typography>
+              Content TODO
+            </Typography>
+          </main>
+        </div>
+      );
+    }
+
+    handleDrawerToggle = () => {
+      this.setState({
+        mobileDrawerOpen: !this.state.mobileDrawerOpen,
+      });
+    }
   }
-}
+)));
 
 export default Wallet;
