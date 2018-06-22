a02b18f0d2a6b89af41ce96fd97b3e5e827a391e
Mart Roosmaa <mart@roosmaa.net>
2018-06-22 12:54:18 +0100

    Add pretty tx summary section to account overview

--- src/containers/AccountOverview.tsx
+++ src/containers/AccountOverview.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import { Theme, createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import AccountOverviewHeader from '../components/AccountOverviewHeader';
+import TxDetailsExpansionPanel from '../components/TxDetailsExpansionPanel';
 
 const styles = (theme: Theme) => createStyles({
   content: {
     padding: theme.spacing.unit * 3,
+  },
+  date_group_title: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit,
+    ['&:first-child']: {
+      marginTop: 0,
+    },
   },
 });
 
@@ -29,14 +36,81 @@ const AccountOverview = stylesDecorator(
             balance_in_fiat="~123.99 USD"
           />
           <div className={classes.content}>
-            <Typography>
-              Content TODO
+            <Typography
+              className={classes.date_group_title}
+              variant="body2"
+              color="textSecondary"
+            >
+              Yesterday
             </Typography>
-            <Paper>
-              <Typography>
-                Transaction
-              </Typography>
-            </Paper>
+            <div>
+              <TxDetailsExpansionPanel
+                tx={{
+                  kind: 'receive',
+                  sender_alias: 'John Wick',
+                  sender_address: '5965187292146641611R',
+                  amount: 20.33,
+                }}
+              />
+              <TxDetailsExpansionPanel
+                tx={{
+                  kind: 'send',
+                  recipient_alias: 'John Wick',
+                  recipient_address: '5965187292146641611R',
+                  amount: 1220.33,
+                }}
+              />
+            </div>
+            <Typography
+              className={classes.date_group_title}
+              variant="body2"
+              color="textSecondary"
+            >
+              21st of June
+            </Typography>
+            <div>
+              <TxDetailsExpansionPanel
+                tx={{
+                  kind: 'receive',
+                  sender_alias: 'John Wick',
+                  sender_address: '5965187292146641611R',
+                  amount: 20.33,
+                }}
+              />
+              <TxDetailsExpansionPanel
+                tx={{
+                  kind: 'send',
+                  recipient_alias: 'John Wick',
+                  recipient_address: '5965187292146641611R',
+                  amount: 1220.33,
+                }}
+              />
+            </div>
+            <Typography
+              className={classes.date_group_title}
+              variant="body2"
+              color="textSecondary"
+            >
+              3rd of June
+            </Typography>
+            <div>
+              <TxDetailsExpansionPanel
+                tx={{
+                  kind: 'receive',
+                  sender_alias: 'John Wick',
+                  sender_address: '5965187292146641611R',
+                  amount: 20.33,
+                }}
+              />
+              <TxDetailsExpansionPanel
+                tx={{
+                  kind: 'send',
+                  recipient_alias: 'John Wick',
+                  recipient_address: '5965187292146641611R',
+                  amount: 1220.33,
+                }}
+              />
+            </div>
           </div>
         </React.Fragment>
       );
