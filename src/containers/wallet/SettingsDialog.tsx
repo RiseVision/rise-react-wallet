c0a25ed7ca93e107b04b3e5861efffb55efb7d13
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2018-07-19 13:58:06 -0400

    - focus input after opening a settings dialog - enter should submit - close X should be on the right

--- src/containers/wallet/SettingsDialog.tsx
+++ src/containers/wallet/SettingsDialog.tsx
@@ -136,199 +136,216 @@ class SettingsDialog extends React.Component<Props, State> {
     const { userStore, store, classes } = this.props;
 
     return (
-      <React.Fragment>
-        <ModalPaper open={true} backdrop={BlackBackdrop}>
-          {this.props.field === 'name' && (
-            <React.Fragment>
-              <ModalPaperHeader
-                backButton={true}
-                onBackClick={this.handleBackClick}
-              >
-                <FormattedMessage
-                  id="account-settings.title"
-                  description="New account screen title"
-                  defaultMessage="Update account name"
-                />
-              </ModalPaperHeader>
-              <div className={classes.content}>
-                <p>
-                  Assign a new name to account {userStore!.selectedAccount!.id}.
-                  This name will only be visible to you and nobody else.
-                </p>
-                <div>
-                  <TextField
-                    className={classes.input}
-                    id="account-name"
-                    label="Account name"
-                    value={this.state.name || ''}
-                    onChange={this.handleChange('name')}
-                    margin="normal"
-                  />
-                </div>
-                <div className={classes.footer}>
-                  <Button onClick={this.updateName}>UPDATE NAME</Button>
-                </div>
-              </div>
-            </React.Fragment>
-          )}
-          {this.props.field === 'delegate' && (
-            <React.Fragment>
-              <ModalPaperHeader
-                backButton={true}
-                onBackClick={this.handleBackClick}
-              >
-                <div>
-                  <FormattedMessage
-                    id="account-settings.vote"
-                    description="New account screen title"
-                    defaultMessage="Voted delegate"
-                  />
-                </div>
-              </ModalPaperHeader>
-              <div className={classes.content}>
-                <p>TODO CONTENT</p>
-                <div className={classes.footer}>
-                  <Button onClick={this.updateName}>BUTTON</Button>
-                </div>
-              </div>
-            </React.Fragment>
-          )}
-          {this.props.field === 'fiat' && (
-            <React.Fragment>
-              <ModalPaperHeader
-                backButton={true}
-                onBackClick={this.handleBackClick}
-              >
-                <FormattedMessage
-                  id="account-settings.fiat"
-                  description="New account screen title"
-                  defaultMessage="Displayed FIAT currency"
-                />
-              </ModalPaperHeader>
-              <div className={classes.content}>
-                <p>
-                  Select which FIAT currency you prefer to see your RISE account
-                  value in.
-                </p>
-                <div>
-                  <select
-                    name="fiat"
-                    onChange={this.handleChange('fiat')}
-                    className={classes.input}
-                  >
-                    {store!.config.fiat_currencies.map(name => (
-                      <option key={name} value={name}>
-                        {name}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className={classes.footer}>
-                  <Button onClick={this.updateFiat(false)}>
-                    SET FOR THIS ACCOUNT
-                  </Button>
-                  <Button onClick={this.updateFiat(true)}>
-                    SET FOR ALL ACCOUNTS
-                  </Button>
-                </div>
-              </div>
-            </React.Fragment>
-          )}
-          {this.props.field === 'deletageRegistration' && (
-            <React.Fragment>
-              <ModalPaperHeader
-                backButton={true}
-                onBackClick={this.handleBackClick}
-              >
-                <FormattedMessage
-                  id="account-settings.title"
-                  description="Register..."
-                  defaultMessage="Delegate registration"
-                />
-              </ModalPaperHeader>
-              <div className={classes.content}>
-                <p>TODO CONTENT</p>
-                <div className={classes.footer}>
-                  <Button onClick={this.updateFiat(false)}>BUTTON</Button>
-                </div>
-              </div>
-            </React.Fragment>
-          )}
-          {this.props.field === 'mnemonic2' && (
-            <React.Fragment>
-              <ModalPaperHeader
-                backButton={true}
-                onBackClick={this.handleBackClick}
-              >
-                <FormattedMessage
-                  id="account-settings.mnemonic2"
-                  defaultMessage="Setup 2nd passphrase"
-                />
-              </ModalPaperHeader>
-              <div className={classes.content}>
-                <p>
-                  The second passphrase offers an extra layer of protection for
-                  forgers whose primary mnemonic is stored on servers which can
-                  potentially get hacked and compromised the primary mnemonic.
-                </p>
-                <p>
-                  Once the 2nd passphrase has been set it cannot be changed nor
-                  removed.
-                </p>
-                {userStore!.selectedAccount!.balance < 5 && (
-                  <p className={classes.remove}>
-                    You don't have enough funds on your account to pay the
-                    network fee of 5 RISE to setup a 2nd passphrase!
-                  </p>
-                )}
+      <ModalPaper open={true} backdrop={BlackBackdrop}>
+        {this.props.field === 'name' && (
+          <React.Fragment>
+            <ModalPaperHeader
+              closeButton={true}
+              onCloseClick={this.handleBackClick}
+            >
+              <FormattedMessage
+                id="account-settings.title"
+                description="New account screen title"
+                defaultMessage="Update account name"
+              />
+            </ModalPaperHeader>
+            <div className={classes.content}>
+              <p>
+                Assign a new name to account {userStore!.selectedAccount!.id}.
+                This name will only be visible to you and nobody else.
+              </p>
+              <div>
                 <TextField
                   className={classes.input}
-                  label="2nd passphrase"
-                  onChange={this.handleChange('mnemonic2')}
+                  id="account-name"
+                  label="Account name"
+                  value={this.state.name || ''}
+                  onChange={this.handleChange('name')}
+                  margin="normal"
+                  autoFocus={true}
+                  inputProps={{
+                    onKeyPress: (event: KeyboardEvent) => {
+                      // enter
+                      if (event.charCode === 13) {
+                        this.updateName();
+                      }
+                    }
+                  }}
+                />
+              </div>
+              <div className={classes.footer}>
+                <Button onClick={this.updateName}>UPDATE NAME</Button>
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+        {this.props.field === 'delegate' && (
+          <React.Fragment>
+            <ModalPaperHeader
+              closeButton={true}
+              onCloseClick={this.handleBackClick}
+            >
+              <div>
+                <FormattedMessage
+                  id="account-settings.vote"
+                  description="New account screen title"
+                  defaultMessage="Voted delegate"
+                />
+              </div>
+            </ModalPaperHeader>
+            <div className={classes.content}>
+              <p>TODO CONTENT</p>
+              <div className={classes.footer}>
+                <Button onClick={this.updateName}>BUTTON</Button>
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+        {this.props.field === 'fiat' && (
+          <React.Fragment>
+            <ModalPaperHeader
+              closeButton={true}
+              onCloseClick={this.handleBackClick}
+            >
+              <FormattedMessage
+                id="account-settings.fiat"
+                description="New account screen title"
+                defaultMessage="Displayed FIAT currency"
+              />
+            </ModalPaperHeader>
+            <div className={classes.content}>
+              <p>
+                Select which FIAT currency you prefer to see your RISE account
+                value in.
+              </p>
+              <p>
+                {/* TODO autoFocus={true} */}
+                <select
+                  name="fiat"
+                  onChange={this.handleChange('fiat')}
+                  className={classes.input}
+                >
+                  {store!.config.fiat_currencies.map(name => (
+                    <option key={name} value={name}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
+              </p>
+              <div className={classes.footer}>
+                <Button onClick={this.updateFiat(false)}>
+                  SET FOR THIS ACCOUNT
+                </Button>
+                <Button onClick={this.updateFiat(true)}>
+                  SET FOR ALL ACCOUNTS
+                </Button>
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+        {this.props.field === 'deletageRegistration' && (
+          <React.Fragment>
+            <ModalPaperHeader
+              closeButton={true}
+              onCloseClick={this.handleBackClick}
+            >
+              <FormattedMessage
+                id="account-settings.title"
+                description="Register..."
+                defaultMessage="Delegate registration"
+              />
+            </ModalPaperHeader>
+            <div className={classes.content}>
+              <p>TODO CONTENT</p>
+              <div className={classes.footer}>
+                <Button onClick={this.updateFiat(false)}>BUTTON</Button>
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+        {this.props.field === 'mnemonic2' && (
+          <React.Fragment>
+            <ModalPaperHeader
+              closeButton={true}
+              onCloseClick={this.handleBackClick}
+            >
+              <FormattedMessage
+                id="account-settings.mnemonic2"
+                defaultMessage="Setup 2nd passphrase"
+              />
+            </ModalPaperHeader>
+            <div className={classes.content}>
+              <p>
+                The second passphrase offers an extra layer of protection for
+                forgers whose primary mnemonic is stored on servers which can
+                potentially get hacked and compromised the primary mnemonic.
+              </p>
+              <p>
+                Once the 2nd passphrase has been set it cannot be changed nor
+                removed.
+              </p>
+              {userStore!.selectedAccount!.balance < 5 && (
+                <p className={classes.remove}>
+                  You don't have enough funds on your account to pay the network
+                  fee of 5 RISE to setup a 2nd passphrase!
+                </p>
+              )}
+              <TextField
+                className={classes.input}
+                label="2nd passphrase"
+                onChange={this.handleChange('mnemonic2')}
+                margin="normal"
+                autoFocus={true}
+                inputProps={{
+                  onKeyPress: (event: KeyboardEvent) => {
+                    // enter
+                    if (event.charCode === 13) {
+                      this.updateMnemonic2();
+                    }
+                  }
+                }}
+              />
+              <div className={classes.footer}>
+                <Button onClick={this.updateMnemonic2}>CONTINUE</Button>
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+        {this.props.field === 'removeAccount' && (
+          <React.Fragment>
+            <ModalPaperHeader
+              closeButton={true}
+              onCloseClick={this.handleBackClick}
+            >
+              <FormattedMessage
+                id="account-settings.remove-account"
+                defaultMessage="Remove account"
+              />
+            </ModalPaperHeader>
+            <div className={classes.content}>
+              <p>
+                Are you sure you want to remove{' '}
+                {userStore!.selectedAccount!.name} account ({
+                  userStore!.selectedAccount!.id
+                }) from the wallet? To confirm, enterthe account address in the
+                field below.
+              </p>
+              <div>
+                <TextField
+                  className={classes.input}
+                  label="Account address"
+                  value={this.state.remove || ''}
+                  onChange={this.handleChange('remove')}
                   margin="normal"
                 />
-                <div className={classes.footer}>
-                  <Button onClick={this.updateMnemonic2}>CONTINUE</Button>
-                </div>
               </div>
-            </React.Fragment>
-          )}
-          {this.props.field === 'removeAccount' && (
-            <React.Fragment>
-              <ModalPaperHeader
-                backButton={true}
-                onBackClick={this.handleBackClick}
-              >
-                <FormattedMessage
-                  id="account-settings.remove-account"
-                  defaultMessage="Remove account"
-                />
-              </ModalPaperHeader>
-              <div className={classes.content}>
-                <p>
-                  Are you sure you want to remove{' '}
-                  {userStore!.selectedAccount!.name} account ({
-                    userStore!.selectedAccount!.id
-                  }) from the wallet? To confirm, enterthe account address in
-                  the field below.
-                </p>
-                <div>
-                  <TextField
-                    className={classes.input}
-                    label="Account address"
-                    value={this.state.remove || ''}
-                    onChange={this.handleChange('remove')}
-                    margin="normal"
-                  />
-                </div>
-                <div className={classes.footer}>
-                  <Button onClick={this.removeAccount}>REMOVE ACCOUNT</Button>
-                </div>
+              <div className={classes.footer}>
+                <Button onClick={this.removeAccount}>REMOVE ACCOUNT</Button>
               </div>
-            </React.Fragment>
-          )}
-        </ModalPaper>
-      </React.Fragment>
+            </div>
+          </React.Fragment>
+        )}
+      </ModalPaper>
     );
   }
 }
