a9c3e557299b4d0647bd2511fa7938d313965137
Mart Roosmaa <mart@roosmaa.net>
2018-08-17 15:12:49 +0100

    Quick polish iteration for settings passphrase form.

--- src/components/forms/SettingsPassphraseForm.tsx
+++ src/components/forms/SettingsPassphraseForm.tsx
@@ -1,9 +1,15 @@
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { observer } from 'mobx-react';
 import * as React from 'react';
 import { ChangeEvent, FormEvent } from 'react';
+import {
+  FormattedMessage,
+  InjectedIntlProps,
+  injectIntl,
+} from 'react-intl';
 import { amountToUser } from '../../utils/utils';
 
 interface Props {
@@ -12,12 +18,14 @@ interface Props {
   error?: null | 'already-set' | 'insufficient-funds';
 }
 
+type DecoratedProps = Props & InjectedIntlProps;
+
 export interface State {
   passphrase: string;
 }
 
 @observer
-class SettingsPassphraseForm extends React.Component<Props, State> {
+class SettingsPassphraseForm extends React.Component<DecoratedProps, State> {
   state: State = {
     passphrase: '',
   };
@@ -37,46 +45,108 @@ class SettingsPassphraseForm extends React.Component<Props, State> {
   }
 
   render() {
-    const { error, fee } = this.props;
+    const { intl, error, fee } = this.props;
+
+    const formatAmount = (amount: number) => (
+      `${intl.formatNumber(amountToUser(amount))} RISE`
+    );
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <Typography>
-          The second passphrase offers an extra layer of protection for forgers
-          whose primary mnemonic is stored on servers which can potentially get
-          hacked and compromised the primary mnemonic.
-        </Typography>
-        <Typography>
-          Once the 2nd passphrase has been set it cannot be changed nor removed.
-        </Typography>
-        {!error && (
-          <TextField
-            label="2nd passphrase"
-            onChange={this.handleType}
-            autoFocus={true}
-            fullWidth={true}
-          />
-        )}
-        {error === 'insufficient-funds' && (
-          <Typography color="error">
-            You don't have enough funds on your account to pay the network fee
-            of {amountToUser(fee)} RISE to setup a 2nd passphrase!
+      <Grid
+        container={true}
+        spacing={16}
+        component="form"
+        onSubmit={this.handleSubmit}
+      >
+        <Grid item={true} xs={12}>
+          <Typography>
+            <FormattedMessage
+              id="forms-passphrase.instructions-about"
+              description="Instructions for setup 2nd passphrase form"
+              defaultMessage={
+                'The second passphrase offers an extra layer of protection for forgers ' +
+                'whose primary mnemonic is stored on servers which can potentially get ' +
+                'hacked and compromise the primary mnemonic.'
+              }
+            />
           </Typography>
+        </Grid>
+        <Grid item={true} xs={12}>
+          <Typography>
+            <FormattedMessage
+              id="forms-passphrase.instructions-immutable"
+              description="Warning about the 2nd passphrase being immutable"
+              defaultMessage="Once the 2nd passphrase has been set it cannot be changed nor removed."
+            />
+          </Typography>
+        </Grid>
+        {error === 'insufficient-funds' && (
+          <Grid item={true} xs={12}>
+            <Typography color="error">
+              <FormattedMessage
+                id="forms-passphrase.insufficient-funds-error"
+                description="Error about not having enough funds to setup a passphrase"
+                defaultMessage={
+                  'You don\'t have enough funds on your account to pay the network fee ' +
+                  'of {fee} to setup a 2nd passphrase!'
+                }
+                values={{
+                  fee: formatAmount(fee),
+                }}
+              />
+            </Typography>
+          </Grid>
         )}
         {error === 'already-set' && (
-          <Typography color="error">
-            You've already set the 2nd passphrase, it can't be changed. Transfer
-            you funds to a new address to set a new 2nd passphrase.
-          </Typography>
+          <Grid item={true} xs={12}>
+            <Typography color="error">
+              <FormattedMessage
+                id="forms-passphrase.already-set-error"
+                description="Error about the 2nd passphrase being set already"
+                defaultMessage={
+                  'You\'ve already set a 2nd passphrase for this account. You need to ' +
+                  'create a new account should you wish to change your passphrase.'
+                }
+              />
+            </Typography>
+          </Grid>
         )}
-        <div>
+        {!error && (
+          <Grid item={true} xs={12}>
+            <TextField
+              label={(
+                <FormattedMessage
+                  id="forms-passphrase.passphrase-input-label"
+                  description="Label for 2nd passphrase text field."
+                  defaultMessage="2nd passphrase"
+                />
+              )}
+              onChange={this.handleType}
+              autoFocus={true}
+              fullWidth={true}
+            />
+          </Grid>
+        )}
+        <Grid item={true} xs={12}>
           <Button type="submit" fullWidth={true}>
-            {!error ? 'Continue' : 'Close'}
+            {!error ? (
+              <FormattedMessage
+                id="forms-passphrase.continue-button"
+                description="Label for continue button."
+                defaultMessage="Continue"
+              />
+            ) : (
+              <FormattedMessage
+                id="forms-passphrase.close-button"
+                description="Label for close button."
+                defaultMessage="Close"
+              />
+            )}
           </Button>
-        </div>
-      </form>
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default SettingsPassphraseForm;
+export default injectIntl(SettingsPassphraseForm);
