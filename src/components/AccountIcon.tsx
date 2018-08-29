11f3dac4deb4ee8bbea752a39ca16b797294fd06
Mart Roosmaa <mart@roosmaa.net>
2018-08-29 13:27:43 +0100

    Make AccountIcon component aria-hidden.
    There's no way to accurately describe the image generated for the
    accounts, so it's better to just hide it than introduce noise to the
    screen reader output.

--- src/components/AccountIcon.tsx
+++ src/components/AccountIcon.tsx
@@ -54,6 +54,7 @@ const AccountIcon = stylesDecorator(
             width: size,
             height: size,
           }}
+          aria-hidden={true}
         >
           <svg
             ref={this.svgElement}
