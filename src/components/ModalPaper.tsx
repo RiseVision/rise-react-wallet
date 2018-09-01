RiseVision/rise-react-wallet

commit c9fed3438ab353e5d1552a1cad8e850dc37ae90a

Transform SettingsName into AccountNameDialog

src/components/ModalPaper.tsx
@@ -33,7 +33,10 @@ const styles = createStyles({
   }
 });
 
-interface Props extends WithStyles<typeof styles> {
+type BaseProps = React.HTMLAttributes<HTMLDivElement>
+  & WithStyles<typeof styles>;
+
+interface Props extends BaseProps {
   open: boolean;
   backdrop?: React.ReactType<BackdropProps>;
   onEscapeKeyDown?: React.ReactEventHandler<{}>;
@@ -44,7 +47,15 @@ const stylesDecorator = withStyles(styles, { name: 'ModalPaper' });
 const ModalPaper = stylesDecorator(
   class extends React.Component<Props> {
     render() {
-      const { classes, open, onEscapeKeyDown, children } = this.props;
+      const {
+        classes,
+        open,
+        backdrop,
+        onEscapeKeyDown,
+        children,
+        role,
+        ...other
+      } = this.props;
       const transitionDuration = {
         enter: duration.enteringScreen,
         exit: duration.leavingScreen
@@ -59,11 +70,16 @@ const ModalPaper = stylesDecorator(
           BackdropComponent={this.props.backdrop || ModalBackdrop}
           onEscapeKeyDown={onEscapeKeyDown}
         >
-          <Fade appear={true} in={open} timeout={transitionDuration}>
-            <div className={classes.content}>
+          <div
+            className={classes.content}
+            role={role || 'dialog'}
+            aria-modal={true}
+            {...other}
+          >
+            <Fade appear={true} in={open} timeout={transitionDuration}>
               <Paper className={classes.paper}>{children}</Paper>
-            </div>
-          </Fade>
+            </Fade>
+          </div>
         </Modal>
       );
     }

src/components/ModalPaperHeader.tsx
@@ -46,6 +46,7 @@ const styles = (theme: Theme) => createStyles({
 });
 
 interface Props extends WithStyles<typeof styles> {
+  titleId?: string;
   backButton?: boolean;
   onBackClick?: () => void;
   closeButton?: boolean;
@@ -83,7 +84,14 @@ class ModalPaperHeader extends React.Component<DecoratedProps> {
   }
 
   render() {
-    const { intl, classes, backButton, closeButton, children } = this.props;
+    const {
+      intl,
+      classes,
+      titleId,
+      backButton,
+      closeButton,
+      children,
+    } = this.props;
     return (
       <div className={classes.root}>
         {backButton && (
@@ -98,6 +106,7 @@ class ModalPaperHeader extends React.Component<DecoratedProps> {
           </Button>
         )}
         <Typography
+          id={titleId}
           className={classNames(
             classes.content,
             (!backButton && !!closeButton) && classes.withoutBack,

src/components/dialogs/AccountNameDialog.tsx
@@ -1,0 +1,169 @@
+import Backdrop from '@material-ui/core/Backdrop';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import {
+  createStyles,
+  Theme,
+  WithStyles,
+  withStyles
+} from '@material-ui/core/styles';
+import * as React from 'react';
+import { ChangeEvent, FormEvent } from 'react';
+import {
+  defineMessages,
+  InjectedIntlProps,
+  injectIntl
+} from 'react-intl';
+import ModalPaper from '../ModalPaper';
+import ModalPaperHeader from '../ModalPaperHeader';
+import autoId from '../../utils/autoId';
+
+const styles = (theme: Theme) => createStyles({
+  content: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+  },
+});
+
+const stylesDecorator = withStyles(styles);
+
+const messages = defineMessages({
+  dialogTitle: {
+    id: 'account-name-dialog.dialog-title',
+    description: 'Account name dialog title',
+    defaultMessage: 'Update account name',
+  },
+  instructions: {
+    id: 'account-name-dialog.instructions',
+    description: 'Instructions before the account name input field',
+    defaultMessage: 'Assign a new name to account {address}. ' +
+      'This name will only be visible to you and nobody else.',
+  },
+  nameField: {
+    id: 'account-name-dialog.name-input-label',
+    description: 'Account name text field label',
+    defaultMessage: 'Account name',
+  },
+  updateButton: {
+    id: 'account-name-dialog.update-button-label',
+    description: 'Update account name button label',
+    defaultMessage: 'Update name',
+  },
+});
+
+interface Props extends WithStyles<typeof styles> {
+  open: boolean;
+  account: {
+    name: string;
+    address: string;
+  };
+  onBackClick?: () => void;
+  onCloseClick?: () => void;
+  onChange: (account: { address: string; name: string; }) => void;
+}
+
+type DecoratedProps = Props & InjectedIntlProps;
+
+interface State {
+  name: string;
+}
+
+class AccountNameDialog extends React.Component<DecoratedProps, State> {
+  @autoId dialogTitleId: string;
+  @autoId dialogContentId: string;
+
+  state = {
+    name: '',
+  };
+
+  constructor(props: DecoratedProps) {
+    super(props);
+
+    const { account } = this.props;
+    this.state.name = account.name;
+  }
+
+  handleNameChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    const name = ev.target.value;
+    this.setState({ name });
+  }
+
+  handleFormSubmit = (ev: FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+
+    const { onChange, account } = this.props;
+    const { name } = this.state;
+    onChange({
+      address: account.address,
+      name: name.trim(),
+    });
+  }
+
+  render() {
+    const {
+      intl,
+      classes,
+      open,
+      account,
+      onBackClick,
+      onCloseClick,
+    } = this.props;
+    const {
+      name,
+    } = this.state;
+    return (
+      <ModalPaper
+        open={open}
+        backdrop={Backdrop}
+        aria-labelledby={this.dialogTitleId}
+        aria-describedby={this.dialogContentId}
+        onEscapeKeyDown={onCloseClick}
+      >
+        <ModalPaperHeader
+          titleId={this.dialogTitleId}
+          closeButton={!!onCloseClick}
+          onCloseClick={onCloseClick}
+          backButton={!!onBackClick}
+          onBackClick={onBackClick}
+          children={intl.formatMessage(messages.dialogTitle)}
+        />
+        <Grid
+          className={classes.content}
+          container={true}
+          spacing={16}
+          component="form"
+          onSubmit={this.handleFormSubmit}
+        >
+          <Grid item={true} xs={12}>
+            <Typography
+              id={this.dialogContentId}
+              children={intl.formatMessage(messages.instructions, {
+                address: account.address,
+              })}
+            />
+          </Grid>
+          <Grid item={true} xs={12}>
+            <TextField
+              label={intl.formatMessage(messages.nameField)}
+              autoFocus={true}
+              value={name}
+              onChange={this.handleNameChange}
+              fullWidth={true}
+            />
+          </Grid>
+          <Grid item={true} xs={12}>
+            <Button
+              type="submit"
+              fullWidth={true}
+              children={intl.formatMessage(messages.updateButton)}
+            />
+          </Grid>
+        </Grid>
+      </ModalPaper>
+    );
+  }
+}
+
+export default stylesDecorator(injectIntl(AccountNameDialog));

src/components/forms/SettingsName.tsx
@@ -1,96 +1,0 @@
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import { observer } from 'mobx-react';
-import * as React from 'react';
-import { ChangeEvent, FormEvent } from 'react';
-import { FormattedMessage } from 'react-intl';
-
-interface Props {
-  onSubmit: (state: State) => void;
-  name: string;
-  id: string;
-}
-
-export interface State {
-  name: string | null;
-}
-
-@observer
-class SettingsNameForm extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      ...this.state,
-      name: props.name
-    };
-  }
-
-  // TODO extract to Form
-  handleChange = (field: string) => (
-    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    const value = event.target.value;
-    if (field === 'name') {
-      this.setState({
-        [field]: value
-      });
-    }
-  }
-
-  onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    this.props.onSubmit({ ...this.state });
-  }
-
-  render() {
-    return (
-      <Grid
-        container={true}
-        spacing={16}
-        component="form"
-        onSubmit={this.onSubmit}
-      >
-        <Grid item={true} xs={12}>
-          <Typography>
-            <FormattedMessage
-              id="forms-settings-name.instructions"
-              description="Instructions before the account name input field"
-              defaultMessage={'Assign a new name to account {address}. ' +
-                'This name will only be visible to you and nobody else.'}
-              values={{
-                address: this.props.id,
-              }}
-            />
-          </Typography>
-        </Grid>
-        <Grid item={true} xs={12}>
-          <TextField
-            label={
-              <FormattedMessage
-                id="forms-settings-name.name-input-label"
-                description="Account name input label"
-                defaultMessage="Account name"
-              />
-            }
-            value={this.state.name || ''}
-            onChange={this.handleChange('name')}
-            fullWidth={true}
-          />
-        </Grid>
-        <Grid item={true} xs={12}>
-          <Button type="submit" fullWidth={true}>
-            <FormattedMessage
-              id="forms-settings-name.update"
-              description="Update account name button label"
-              defaultMessage="Update name"
-            />
-          </Button>
-        </Grid>
-      </Grid>
-    );
-  }
-}
-
-export default SettingsNameForm;

src/containers/wallet/Settings.tsx
@@ -27,9 +27,6 @@ import Dialog from '../../components/Dialog';
 import FiatForm, {
   State as FiatState
 } from '../../components/forms/SettingsFiat';
-import NameForm, {
-  State as NameState
-} from '../../components/forms/SettingsName';
 import RemoveAccountForm from '../../components/forms/SettingsRemoveAccountForm';
 import {
   accountOverviewRoute,
@@ -48,6 +45,7 @@ import WalletStore from '../../stores/wallet';
 import SettingsPassphrase from './SettingsPassphrase';
 import RegisterDelegate from './RegisterDelegate';
 import VoteDelegate from './VoteDelegate';
+import AccountNameDialog from '../../components/dialogs/AccountNameDialog';
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -263,9 +261,8 @@ class AccountSettings extends React.Component<DecoratedProps, State> {
   // DIALOG ACTIONS
 
   @action
-  onSubmitName = (state: NameState) => {
-    // TOOD validate state.name
-    this.account.name = state.name!;
+  onSubmitName = (acc: { name: string }) => {
+    this.account.name = acc.name;
     this.onDialogClose();
   }
 
@@ -301,6 +298,21 @@ class AccountSettings extends React.Component<DecoratedProps, State> {
   getDialog: () => ReactElement<HTMLElement> | null = () => {
     let form, title;
     const config = this.injected.walletStore.config;
+    const { dialogField } = this.state;
+
+    if (dialogField === 'name') {
+      return (
+        <AccountNameDialog
+          account={{
+            name: this.account.name,
+            address: this.account.id,
+          }}
+          open={this.state.dialogOpen}
+          onCloseClick={this.onDialogClose}
+          onChange={this.onSubmitName}
+        />
+      );
+    }
 
     switch (this.state.dialogField!) {
       case 'delegate':
@@ -309,21 +321,6 @@ class AccountSettings extends React.Component<DecoratedProps, State> {
         return <RegisterDelegate onSubmit={this.onDialogClose} />;
       case 'passphrase':
         return <SettingsPassphrase onSubmit={this.onDialogClose} />;
-      case 'name':
-        title = (
-          <FormattedMessage
-            id="settings-dialog-title"
-            defaultMessage={'Update account name'}
-          />
-        );
-        form = (
-          <NameForm
-            name={this.account.name || ''}
-            id={this.account.id}
-            onSubmit={this.onSubmitName}
-          />
-        );
-        break;
       case 'removeAccount':
         title = (
           <FormattedMessage

src/utils/autoId.ts
@@ -1,0 +1,16 @@
+let lastAutoId = 0;
+
+// tslint:disable-next-line:no-any
+export default function autoId(target: any, key: string) {
+    Object.defineProperty(target, key, {
+        get: function () {
+            if (this.__autoId === undefined) {
+                lastAutoId += 1;
+                this.__autoId = lastAutoId;
+            }
+            return `${key}-${this.__autoId}`;
+        },
+        configurable: true,
+        enumerable: true,
+    });
+}

commit be6e92dca58f685d9741179d8f0ccda30f6648f7

Make sure Modal doesn't miss that there's a transition in ModalPaper
Fixes regression introduced in c9fed343, which made the Modal component
miss the fact that transitions are involved.

src/components/ModalPaper.tsx
@@ -70,16 +70,26 @@ const ModalPaper = stylesDecorator(
           BackdropComponent={this.props.backdrop || ModalBackdrop}
           onEscapeKeyDown={onEscapeKeyDown}
         >
-          <div
-            className={classes.content}
-            role={role || 'dialog'}
-            aria-modal={true}
-            {...other}
+          <Fade
+            appear={true}
+            in={open}
+            timeout={transitionDuration}
+            {...{
+              // Fade component passes all extra props to its children,
+              // but TS typedef don't reflect that. Modal component does
+              // various detections based on it's child props, so this is
+              // a hacky way around the TS compiler to make everything work.
+              role: role || 'dialog',
+            }}
           >
-            <Fade appear={true} in={open} timeout={transitionDuration}>
+            <div
+              className={classes.content}
+              aria-modal={true}
+              {...other}
+            >
               <Paper className={classes.paper}>{children}</Paper>
-            </Fade>
-          </div>
+            </div>
+          </Fade>
         </Modal>
       );
     }