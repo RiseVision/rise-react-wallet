2a31d6d41375643012c953f48afabe2cc72e4df5
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2018-09-05 14:20:49 -0400

    tests for dialog title buttons assert backend requests test for account switching

--- cypress/integration/wallet.spec.js
+++ cypress/integration/wallet.spec.js
@@ -15,6 +15,15 @@ const fixtures = {
     fiatCurrency: 'USD',
     name: null,
     pinned: false
+  },
+  account2: {
+    id: '10317456780953445784R',
+    publicKey:
+      'e9ae239743b47125305a3f339937661368a7f8d810ae53d79e5c4de001356563',
+    readOnly: false,
+    fiatCurrency: 'USD',
+    name: null,
+    pinned: false
   }
 };
 
@@ -92,12 +101,19 @@ function closeDialog() {
     .click();
 }
 
+function getStorageAccount(id) {
+  return lstore.get('accounts').find(a => a.id === id);
+}
+
 beforeEach(() => {
   cy.visit('http://localhost:3000/').then(() => {
-    lstore.set('accounts', [fixtures.account]);
+    lstore.set('accounts', [fixtures.account, fixtures.account2]);
     lstore.set('lastSelectedAccount', fixtures.account.id);
     cy.visit('http://localhost:3000/');
   });
+  // mock the server
+  cy.server();
+  cy.route('POST', '**/peer/transactions').as('postTransaction');
 });
 
 afterEach(() => {
@@ -124,14 +140,32 @@ context('Wallet', () => {
     getDialog()
       .contains('span', 'successfully')
       .should('have.length', 1);
-    // TODO assert the backend
-    // getDialog().find('button').eq(0).click()
+    // assert the request
+    cy.wait('@postTransaction')
+      .its('status')
+      .should('eq', 200);
+  });
+
+  it('switch between accounts', () => {
+    // click the menu
+    cy.get('ul[aria-label="Accounts"]')
+      .find('p')
+      .contains(fixtures2.id)
+      .click();
+    // assert the header
+    cy.get('main > :nth-child(2)')
+      .find('p')
+      .contains(fixtures2.id)
+      .should('have.length', 1);
   });
 });
 
 context('Settings', () => {
-  it('vote delegate', () => {
+  beforeEach(() => {
     goToSettings();
+  });
+
+  it('vote delegate', () => {
     clickSettingsRow('Voted delegate');
     assertAutofocus();
     // type in the query
@@ -144,36 +178,37 @@ context('Settings', () => {
     // pick the first result (2nd button)
     clickDialogButton(1);
     fillConfirmationDialog(fixtures.mnemonic, fixtures.passphrase);
-    // TODO assert the backend
-    // getDialog().find('button').eq(0).click()
+    // assert the request
+    cy.wait('@postTransaction')
+      .its('status')
+      .should('eq', 200);
   });
 
   it('account name', () => {
-    goToSettings();
     clickSettingsRow('Account name');
     assertAutofocus();
     // type in the query
     const newName = 'new name ' + Math.random();
     fillDialogInput(0, newName);
-    clickDialogButton(1)
-      .then(_ => cy.wait(1000))
-      .then(_ => {
-        expect(lstore.get('accounts')[0].name).to.eql(newName);
-      });
+    clickDialogButton(1).then(_ => {
+      const account = getStorageAccount(fixtures.account.id);
+      expect(account.name).to.eql(newName);
+      // wait for observables to settle
+      cy.wait(1000);
+    });
   });
 
   it('pinned', () => {
-    goToSettings();
-    clickSettingsRow('Pinned')
-      .then(_ => cy.wait(1000))
-      .then(_ => {
-        console.log(lstore.get('accounts')[0])
-        expect(lstore.get('accounts')[0].pinned).to.eql(true);
-      });
+    cy.wait(1000);
+    clickSettingsRow('Pinned').then(_ => {
+      const account = getStorageAccount(fixtures.account.id);
+      expect(account.pinned).to.eql(true);
+      // wait for observables to settle
+      cy.wait(1000);
+    });
   });
 
   it('FIAT', () => {
-    goToSettings();
     clickSettingsRow('FIAT');
     getDialog()
       .find('select')
@@ -181,7 +216,8 @@ context('Settings', () => {
     clickDialogButton(2)
       .then(_ => cy.wait(1000))
       .then(_ => {
-        expect(lstore.get('accounts')[0].fiatCurrency).to.eql('EUR');
+        const account = getStorageAccount(fixtures.account.id);
+        expect(account.fiatCurrency).to.eql('EUR');
       });
   });
 });
@@ -227,5 +263,52 @@ context('Wallet errors', () => {
       .contains('p', 'Incorrect secret')
       .should('have.length', 1);
     closeDialog();
+  });
+});
+
+context('Dialog title buttons', () => {
+  it('go back to the first form', () => {
+    // click the Send RISE button
+    cy.get('button[title="Send RISE"]').click();
+    // type in the recipient address
+    fillDialogInput(0, fixtures2.id);
+    // type in the amount
+    fillDialogInput(1, '1');
+    // click submit
+    clickDialogSubmit();
+    // click the back button
+    clickDialogButton(0);
+    getDialog()
+      .contains('h1', 'Send RISE')
+      .should('have.length', 1);
+    closeDialog();
+  });
+
+  it('close button on the first form', () => {
+    // click the Send RISE button
+    cy.get('button[title="Send RISE"]').click();
+    // click the close button
+    clickDialogButton(0);
+    // assert that the dialog is gone
+    cy.get('body')
+      .find('div[role="dialog"]')
+      .should('not.exist');
+  });
+
+  it('close button on the second form', () => {
+    // click the Send RISE button
+    cy.get('button[title="Send RISE"]').click();
+    // type in the recipient address
+    fillDialogInput(0, fixtures2.id);
+    // type in the amount
+    fillDialogInput(1, '1');
+    // click submit
+    clickDialogSubmit();
+    // click the close button
+    clickDialogButton(1);
+    // assert that the dialog is gone
+    cy.get('body')
+      .find('div[role="dialog"]')
+      .should('not.exist');
   });
 });
