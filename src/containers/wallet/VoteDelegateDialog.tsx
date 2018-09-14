RiseVision/rise-react-wallet

commit 8aced0b08b631ede2d1b805f5a74dd7f918212bc

Migrate vote delegate to Dialog and DialogContent combo.

src/containers/wallet/VoteDelegateDialog.tsx
@@ -1,0 +1,457 @@
+import * as assert from 'assert';
+import { Delegate } from 'dpos-api-wrapper';
+import { throttle, sampleSize } from 'lodash';
+import { reaction, IReactionDisposer } from 'mobx';
+import { inject, observer } from 'mobx-react';
+import { RouterStore } from 'mobx-router';
+import * as React from 'react';
+import ConfirmTransactionDialogContent from '../../components/content/ConfirmTransactionDialogContent';
+import VoteDelegateDialogContent from '../../components/content/VoteDelegateDialogContent';
+import { accountSettingsVoteRoute } from '../../routes';
+import Dialog, { DialogProps } from '../../components/Dialog';
+import AccountStore, { LoadingState } from '../../stores/account';
+import WalletStore from '../../stores/wallet';
+
+interface Props {
+  account: AccountStore;
+  onNavigateBack: () => void;
+}
+
+interface PropsInjected extends Props {
+  routerStore: RouterStore;
+  walletStore: WalletStore;
+}
+
+interface State {
+  activeDelegates: null | Delegate[];
+  step:
+    | 'vote'
+    | 'confirm'
+    | 'sending'
+    | 'failure'
+    | 'sent';
+  query: string;
+  search: {
+    isLoading: boolean;
+    query: string;
+    delegates: Delegate[];
+  };
+  selectedDelegate: null | Delegate;
+  secrets: {
+    mnemonic: string;
+    passphrase: null | string;
+  };
+  sendError: string;
+}
+
+const EMPTY_SECRETS = {
+  mnemonic: '',
+  passphrase: null,
+};
+
+@inject('routerStore')
+@inject('walletStore')
+@observer
+class VoteDelegateDialog extends React.Component<Props, State> {
+  disposeOpenMonitor: null | IReactionDisposer = null;
+  lastSearch = 0;
+  state: State = {
+    activeDelegates: null,
+    step: 'vote',
+    query: '',
+    search: {
+      isLoading: true,
+      query: '',
+      delegates: [],
+    },
+    selectedDelegate: null,
+    secrets: EMPTY_SECRETS,
+    sendError: '',
+  };
+
+  searchDelegates = throttle(async (query: string) => {
+    const thisSearch = ++this.lastSearch;
+    const { walletStore } = this.injected;
+    query = query.trim();
+
+    this.setState({
+      search: {
+        isLoading: true,
+        query,
+        delegates: []
+      }
+    });
+
+    const result = await walletStore.searchDelegates(query);
+    // Make sure that the results are still something that the user is interested in
+    if (this.lastSearch !== thisSearch) {
+      return;
+    }
+
+    this.setState({
+      search: {
+        isLoading: false,
+        query,
+        delegates: result.slice(0, 3)
+      }
+    });
+  });
+
+  get injected(): PropsInjected {
+    return this.props as PropsInjected;
+  }
+
+  handleClose = () => {
+    const { onNavigateBack } = this.injected;
+
+    // Clear secrets from state when closing
+    this.setState({
+      secrets: EMPTY_SECRETS,
+    });
+
+    onNavigateBack();
+  }
+
+  handleQueryChange = (query: string) => {
+    this.setState({ query });
+    if (query.trim()) {
+      this.searchDelegates(query);
+    } else {
+      this.suggestDelegates();
+    }
+  }
+
+  handleSelectDelegate = (delegate: Delegate) => {
+    this.setState({
+      step: 'confirm',
+      selectedDelegate: delegate,
+    });
+  }
+
+  handleBackFromConfirm = () => {
+    this.setState({
+      step: 'vote',
+    });
+  }
+
+  handleConfirmTransaction = (secrets: State['secrets']) => {
+    this.sendTransaction(secrets);
+  }
+
+  handleRetryTransaction = () => {
+    const { secrets } = this.state;
+    this.sendTransaction(secrets);
+  }
+
+  async suggestDelegates() {
+    const thisSearch = ++this.lastSearch;
+    const { walletStore } = this.injected;
+
+    let { activeDelegates } = this.state;
+    if (activeDelegates === null) {
+      this.setState({
+        search: {
+          isLoading: true,
+          query: '',
+          delegates: []
+        }
+      });
+      const response = await walletStore.dposAPI.delegates.getList();
+      activeDelegates = response.delegates;
+    }
+    // Make sure that the suggestions are still something that the user is interested in
+    if (this.lastSearch !== thisSearch) {
+      return;
+    }
+
+    this.setState({
+      activeDelegates: activeDelegates,
+      search: {
+        isLoading: false,
+        query: '',
+        delegates: sampleSize(activeDelegates, 3)
+      }
+    });
+  }
+
+  async sendTransaction(secrets: State['secrets']) {
+    const { account, walletStore } = this.injected;
+    const { selectedDelegate } = this.state;
+
+    assert(selectedDelegate, 'Delegate selection required');
+
+    this.setState({ step: 'sending' });
+
+    let success = false;
+    let errorSummary = '';
+    let canRetry = false;
+    try {
+      const tx = await walletStore.voteTransaction(
+        selectedDelegate!.publicKey,
+        secrets.mnemonic,
+        secrets.passphrase,
+        account.id
+      );
+      success = tx.success;
+      // TODO error msg
+      errorSummary = '';
+      // If the node rejected the transaction there's no point in retrying
+      canRetry = false;
+    } catch (e) {
+      success = false;
+      // TODO: Network errors should be safe to retry. But we cannot do that because
+      //       there's a failure case where it isn't safe currently - when the request
+      //       goes through, but network is cut out mid-response. Sending the transaction
+      //       again currently means we generate and sign a new transaction and that can
+      //       potentially cause loss of funds. Retry will be safe if we resend the same
+      //       data blob that we produced on first signing.
+      canRetry = false;
+      errorSummary = e.toString();
+    }
+
+    if (success) {
+      this.setState({
+        step: 'sent',
+        secrets: EMPTY_SECRETS,
+      });
+    } else {
+      // TODO: Really wish we would not store the secrets in memory for extended periods of time,
+      //       instead the transaction should be prepared and then if retry is required just sent
+      //       again.
+      this.setState({
+        step: 'failure',
+        secrets: canRetry ? secrets : EMPTY_SECRETS,
+        sendError: errorSummary,
+      });
+    }
+  }
+
+  resetState() {
+    this.setState({
+      query: '',
+      step: 'vote',
+      selectedDelegate: null,
+      secrets: EMPTY_SECRETS,
+      sendError: '',
+    });
+    this.suggestDelegates();
+
+    // load the current vote (settings should preload)
+    const { account, walletStore } = this.injected;
+    if (account.votedDelegateState === LoadingState.NOT_LOADED) {
+      walletStore.loadVotedDelegate(account.id);
+    }
+  }
+
+  componentWillMount() {
+    this.disposeOpenMonitor = reaction(() => this.isOpen, (isOpen) => {
+      if (isOpen) {
+        this.resetState();
+      }
+    });
+
+    this.resetState();
+  }
+
+  componentWillUnmount() {
+    if (this.disposeOpenMonitor) {
+      this.disposeOpenMonitor();
+      this.disposeOpenMonitor = null;
+    }
+  }
+
+  get isOpen() {
+    const { routerStore } = this.injected;
+    return routerStore.currentView === accountSettingsVoteRoute;
+  }
+
+  get voteFee() {
+    const { walletStore } = this.injected;
+    return  walletStore.fees.get('vote')!;
+  }
+
+  render() {
+    const { onClose, onNavigateBack, content } = this.renderStep();
+
+    return (
+      <Dialog
+        open={this.isOpen}
+        onClose={onClose}
+        onNavigateBack={onNavigateBack}
+        children={content}
+      />
+    );
+  }
+
+  renderStep(): {
+    onClose?: DialogProps['onClose'],
+    onNavigateBack?: DialogProps['onNavigateBack'],
+    content: DialogProps['children'],
+  } {
+    const { step } = this.state;
+
+    switch (step) {
+      default:
+        return {
+          onClose: this.handleClose,
+          content: this.renderVoteContent(),
+        };
+      case 'confirm':
+        return {
+          onClose: this.handleClose,
+          onNavigateBack: this.handleBackFromConfirm,
+          content: this.renderConfirmTxContent(),
+        };
+      case 'sending':
+        return {
+          content: this.renderSendingTxContent(),
+        };
+      case 'failure':
+        return {
+          onClose: this.handleClose,
+          content: this.renderFailedTxContent(),
+        };
+      case 'sent':
+        return {
+          onClose: this.handleClose,
+          content: this.renderSentTxContent(),
+        };
+    }
+  }
+
+  renderVoteContent() {
+    const { account } = this.injected;
+    const { votedDelegate, balance } = account;
+    const { query, search } = this.state;
+
+    const fee = this.voteFee;
+    const hasInsufficientFunds = balance.lt(fee);
+
+    const showSuggestions = search.query === '';
+
+    return (
+      <VoteDelegateDialogContent
+        query={query}
+        onQueryChange={this.handleQueryChange}
+        onSelect={this.handleSelectDelegate}
+        isLoading={search.isLoading}
+        votedDelegate={votedDelegate}
+        voteFee={fee}
+        content={
+          hasInsufficientFunds ? {
+            kind: 'insufficient-funds',
+            onClose: this.handleClose,
+          }
+          : showSuggestions ? {
+            kind: 'suggestions',
+            delegates: search.delegates,
+          }
+          : {
+            kind: 'search-results',
+            query: search.query,
+            delegates: search.delegates,
+          }
+        }
+      />
+    );
+  }
+
+  confirmDialogData() {
+    const { account } = this.injected;
+    const { votedDelegate } = account;
+    const { selectedDelegate } = this.state;
+
+    let removeNames = [];
+    let addNames = [];
+
+    const isRemoveTx =
+      votedDelegate && selectedDelegate && votedDelegate.publicKey === selectedDelegate.publicKey;
+    if (votedDelegate) {
+      removeNames.push(votedDelegate.username);
+    }
+    if (!isRemoveTx && selectedDelegate) {
+      addNames.push(selectedDelegate.username);
+    }
+    return {
+      kind: 'vote' as 'vote',
+      remove: removeNames,
+      add: addNames,
+    };
+  }
+
+  renderConfirmTxContent() {
+    const { account } = this.injected;
+
+    return (
+      <ConfirmTransactionDialogContent
+        data={this.confirmDialogData()}
+        fee={this.voteFee}
+        senderName={account.name}
+        senderAddress={account.id}
+        step={{
+          kind: 'confirm',
+          publicKey: account.publicKey,
+          secondPublicKey: account.secondPublicKey,
+          onConfirm: this.handleConfirmTransaction
+        }}
+      />
+    );
+  }
+
+  renderSendingTxContent() {
+    const { account } = this.injected;
+
+    return (
+      <ConfirmTransactionDialogContent
+        data={this.confirmDialogData()}
+        fee={this.voteFee}
+        senderName={account.name}
+        senderAddress={account.id}
+        step={{
+          kind: 'in-progress'
+        }}
+      />
+    );
+  }
+
+  renderFailedTxContent() {
+    const { account } = this.injected;
+    const { secrets, sendError } = this.state;
+
+    const canRetry = !!secrets.mnemonic;
+
+    return (
+      <ConfirmTransactionDialogContent
+        data={this.confirmDialogData()}
+        fee={this.voteFee}
+        senderName={account.name}
+        senderAddress={account.id}
+        step={{
+          kind: 'failure',
+          reason: sendError,
+          onRetry: canRetry ? this.handleRetryTransaction : undefined,
+          onClose: this.handleClose,
+        }}
+      />
+    );
+  }
+
+  renderSentTxContent() {
+    const { account } = this.injected;
+
+    return (
+      <ConfirmTransactionDialogContent
+        data={this.confirmDialogData()}
+        fee={this.voteFee}
+        senderName={account.name}
+        senderAddress={account.id}
+        step={{
+          kind: 'success',
+          onClose: this.handleClose,
+        }}
+      />
+    );
+  }
+}
+
+export default VoteDelegateDialog;

src/components/Dialog.tsx
@@ -18,7 +18,7 @@ const styles = (theme: Theme) => createStyles({
 type BaseProps = MuiDialogProps
   & WithStyles<typeof styles>;
 
-interface Props extends BaseProps {
+export interface DialogProps extends BaseProps {
   onNavigateBack?: ReactEventHandler<{}>;
   children: ReactElement<DialogContentProps>;
 }
@@ -30,7 +30,7 @@ interface State {
 
 const stylesDecorator = withStyles(styles);
 
-class Dialog extends React.PureComponent<Props, State> {
+class Dialog extends React.PureComponent<DialogProps, State> {
   @autoId dialogTitleId: string;
   @autoId dialogContentId: string;
 

src/components/content/VoteDelegateDialogContent.tsx
@@ -1,0 +1,224 @@
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import {
+  createStyles,
+  Theme,
+  WithStyles,
+  withStyles
+} from '@material-ui/core/styles';
+import { Delegate } from 'dpos-api-wrapper';
+import { range } from 'lodash';
+import * as React from 'react';
+import { ReactEventHandler } from 'react';
+import { ChangeEvent } from 'react';
+import {
+  FormattedMessage,
+  defineMessages,
+  InjectedIntlProps,
+  injectIntl
+} from 'react-intl';
+import { DialogContentProps, SetDialogContent } from '../Dialog';
+import autoId from '../../utils/autoId';
+import { RawAmount } from '../../utils/amounts';
+import DelegateVoteComponent from '../DelegateVoteComponent';
+
+const styles = (theme: Theme) => createStyles({
+  content: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center'
+  }
+});
+
+const stylesDecorator = withStyles(styles, { name: 'VoteDelegateDialogContent' });
+
+const messages = defineMessages({
+  dialogTitle: {
+    id: 'vote-delegate-dialog-content.dialog-title',
+    description: 'Vote delegate name dialog title',
+    defaultMessage: 'Vote for delegate'
+  },
+  instructions: {
+    id: 'vote-delegate-dialog-content.instructions',
+    description: 'Instructions before the delegate voting section',
+    defaultMessage:
+      'New blocks on the RISE blockchain are forged by the top 101 ' +
+      'delegates. You as the user determine who those delegates are by ' +
+      'casting a vote.'
+  },
+});
+
+type SuggestionsContent = {
+  kind: 'suggestions';
+  delegates: Delegate[];
+};
+
+type ResultsContent = {
+  kind: 'search-results';
+  query: string;
+  delegates: Delegate[];
+};
+
+type ErrorContent = {
+  kind: 'insufficient-funds';
+  onClose: ReactEventHandler<{}>;
+};
+
+type Content =
+  | SuggestionsContent
+  | ResultsContent
+  | ErrorContent;
+
+type BaseProps = WithStyles<typeof styles>
+  & DialogContentProps;
+
+interface Props extends BaseProps {
+  query: string;
+  onQueryChange: (query: string) => void;
+  onSelect: (delegate: Delegate) => void;
+  isLoading: boolean;
+  votedDelegate: null | Delegate;
+  voteFee: RawAmount;
+  content: Content;
+}
+
+type DecoratedProps = Props & InjectedIntlProps;
+
+class VoteDelegateDialogContent extends React.Component<DecoratedProps> {
+  @autoId dialogContentId: string;
+
+  handleQueryChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    const query = ev.target.value;
+    const { onQueryChange } = this.props;
+    onQueryChange(query);
+  }
+
+  componentWillMount() {
+    const { intl } = this.props;
+
+    SetDialogContent(this, {
+      title: intl.formatMessage(messages.dialogTitle),
+      contentId: this.dialogContentId,
+    });
+  }
+
+  render() {
+    const {
+      intl,
+      classes,
+      query,
+      onSelect,
+      isLoading,
+      votedDelegate,
+      voteFee,
+      content,
+    } = this.props;
+
+    const formatAmount = (amount: RawAmount) =>
+      `${intl.formatNumber(amount.unit.toNumber())} RISE`;
+
+    return (
+      <Grid
+        className={classes.content}
+        container={true}
+        spacing={16}
+      >
+        <Grid item={true} xs={12}>
+          <Typography
+            id={this.dialogContentId}
+            children={intl.formatMessage(messages.instructions)}
+          />
+        </Grid>
+        {content.kind === 'insufficient-funds' && (
+          <React.Fragment>
+            <Grid item={true} xs={12}>
+              <Typography color="error">
+                <FormattedMessage
+                  id="vote-delegate-dialog-content.insufficient-funds-error"
+                  description="Error about not having enough funds to vote for a delegate"
+                  defaultMessage={
+                    'You don\'t have enough funds in your account to pay the' +
+                    ' network fee of {fee} for casting a vote for a delegate!'
+                  }
+                  values={{
+                    fee: formatAmount(voteFee)
+                  }}
+                />
+              </Typography>
+            </Grid>
+            <Grid item={true} xs={12}>
+              <Button fullWidth={true} onClick={content.onClose}>
+                <FormattedMessage
+                  id="vote-delegate-dialog-content.close-button"
+                  description="Label for close button."
+                  defaultMessage="Close"
+                />
+              </Button>
+            </Grid>
+          </React.Fragment>
+        )}
+        {(content.kind === 'suggestions' || content.kind === 'search-results') && (
+          <React.Fragment>
+            <Grid item={true} xs={12}>
+              <TextField
+                label={
+                  <FormattedMessage
+                    id="vote-delegate-dialog-content.search-box-label"
+                    description="Delegate search box label"
+                    defaultMessage="Find delegates by username or address"
+                  />
+                }
+                value={query}
+                fullWidth={true}
+                onChange={this.handleQueryChange}
+              />
+            </Grid>
+            <Grid item={true} xs={12}>
+              <Typography variant="body2" color="textSecondary" align="left">
+                {content.kind === 'search-results' ? (
+                  <FormattedMessage
+                    id="vote-delegate-dialog-content.results-subtitle"
+                    description="Title for search results"
+                    defaultMessage={'Search results for "{query}"'}
+                    values={{
+                      query: content.query,
+                    }}
+                  />
+                ) : (
+                  <FormattedMessage
+                    id="vote-delegate-dialog-content.suggested-subtitle"
+                    description="Title for suggested delegates"
+                    defaultMessage="Suggested delegates"
+                  />
+                )}
+              </Typography>
+            </Grid>
+            {range(3).map(n => {
+              const delegate = content.delegates[n] || null;
+              const hasVote = delegate && votedDelegate
+                ? delegate.publicKey === votedDelegate.publicKey
+                : false;
+              return (
+                <Grid
+                  key={delegate ? delegate.address : `placeholder-${n}`}
+                  item={true}
+                  xs={12}
+                >
+                  <DelegateVoteComponent
+                    delegate={delegate}
+                    onSubmit={() => onSelect(delegate)}
+                    hasVote={hasVote}
+                    isLoading={isLoading}
+                  />
+                </Grid>
+              );
+            })}
+          </React.Fragment>
+        )}
+      </Grid>
+    );
+  }
+}
+
+export default stylesDecorator(injectIntl(VoteDelegateDialogContent));

src/components/forms/VoteDelegateForm.tsx
@@ -1,171 +1,0 @@
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import { Delegate } from 'dpos-api-wrapper';
-import { range } from 'lodash';
-import { observer } from 'mobx-react';
-import * as React from 'react';
-import { ChangeEvent } from 'react';
-import { FormattedMessage, InjectedIntlProps, injectIntl } from 'react-intl';
-import { RawAmount } from '../../utils/amounts';
-import DelegateVoteComponent from '../DelegateVoteComponent';
-
-interface Props {
-  onSubmit: (delegate: Delegate, addVote: boolean) => void;
-  onClose: () => void;
-  onSearch: (query: string) => void;
-  isLoading: boolean;
-  isSearch: boolean;
-  delegates: Delegate[];
-  votedDelegate: string | null;
-  fee: RawAmount;
-  query?: string;
-  error?: null | 'insufficient-funds';
-}
-
-type DecoratedProps = Props & InjectedIntlProps;
-
-export interface State {
-  query: string;
-}
-
-@observer
-class VoteDelegateForm extends React.Component<DecoratedProps, State> {
-  state: State = {
-    query: ''
-  };
-
-  constructor(props: DecoratedProps) {
-    super(props);
-    this.state.query = props.query || '';
-  }
-
-  handleType = () => (
-    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    const query = event.target.value.trim().toLowerCase();
-    this.setState({ query });
-    this.props.onSearch(query);
-  }
-
-  render() {
-    const {
-      intl,
-      delegates,
-      votedDelegate,
-      isLoading,
-      isSearch,
-      error,
-      fee
-    } = this.props;
-    const { query } = this.state;
-
-    const formatAmount = (amount: RawAmount) =>
-      `${intl.formatNumber(amount.unit.toNumber())} RISE`;
-
-    return (
-      <React.Fragment>
-        <Grid container={true} spacing={16}>
-          <Grid item={true} xs={12}>
-            <Typography>
-              <FormattedMessage
-                id="forms-vote-delegate.instructions"
-                description="Instructions before the delegate search field"
-                defaultMessage={
-                  'New blocks on the RISE blockchain are forged by the top ' +
-                  '101 delegates. You as the user determine who those' +
-                  ' delegates are by casting a vote.'
-                }
-              />
-            </Typography>
-          </Grid>
-          {error === 'insufficient-funds' && (
-            <React.Fragment>
-              <Grid item={true} xs={12}>
-                <Typography color="error">
-                  <FormattedMessage
-                    id="forms-vote-delegate.insufficient-funds-error"
-                    description="Error about not having enough funds to vote for a delegate"
-                    defaultMessage={
-                      'You don\'t have enough funds in your account to pay the' +
-                      ' network fee of {fee} for casting a vote for a delegate!'
-                    }
-                    values={{
-                      fee: formatAmount(fee)
-                    }}
-                  />
-                </Typography>
-              </Grid>
-              <Grid item={true} xs={12}>
-                <Button fullWidth={true} onClick={this.props.onClose}>
-                  <FormattedMessage
-                    id="forms-vote-delegate.close-button"
-                    description="Label for close button."
-                    defaultMessage="Close"
-                  />
-                </Button>
-              </Grid>
-            </React.Fragment>
-          )}
-          {!error && (
-            <React.Fragment>
-              <Grid item={true} xs={12}>
-                <TextField
-                  label={
-                    <FormattedMessage
-                      id="forms-vote-delegate.search-box-label"
-                      description="Delegate search box label"
-                      defaultMessage="Find delegates by username or address"
-                    />
-                  }
-                  value={query}
-                  fullWidth={true}
-                  onChange={this.handleType()}
-                />
-              </Grid>
-              <Grid item={true} xs={12}>
-                <Typography variant="body2" color="textSecondary" align="left">
-                  {isSearch ? (
-                    <FormattedMessage
-                      id="forms-vote-delegate.results-title"
-                      description="Title for search results"
-                      defaultMessage="Search results"
-                    />
-                  ) : (
-                    <FormattedMessage
-                      id="forms-vote-delegate.suggested-title"
-                      description="Title for suggested delegates"
-                      defaultMessage="Suggested delegates"
-                    />
-                  )}
-                </Typography>
-              </Grid>
-              {range(3).map(n => {
-                const delegate = delegates[n] || null;
-                return (
-                  <Grid
-                    key={delegate ? delegate.address : `placeholder-${n}`}
-                    item={true}
-                    xs={12}
-                  >
-                    <DelegateVoteComponent
-                      delegate={delegate}
-                      onSubmit={this.props.onSubmit}
-                      hasVote={
-                        delegate ? votedDelegate === delegate.publicKey : false
-                      }
-                      isLoading={isLoading}
-                    />
-                  </Grid>
-                );
-              })}
-            </React.Fragment>
-          )}
-        </Grid>
-      </React.Fragment>
-    );
-  }
-}
-
-export default injectIntl(VoteDelegateForm);

src/containers/wallet/Settings.tsx
@@ -44,7 +44,7 @@ import AccountStore, { LoadingState } from '../../stores/account';
 import WalletStore from '../../stores/wallet';
 import SettingsPassphrase from './SettingsPassphrase';
 import RegisterDelegate from './RegisterDelegate';
-import VoteDelegate from './VoteDelegate';
+import VoteDelegateDialog from './VoteDelegateDialog';
 import AccountNameDialog from './AccountNameDialog';
 
 const styles = (theme: Theme) =>
@@ -295,8 +295,6 @@ class AccountSettings extends React.Component<DecoratedProps, State> {
     const { dialogField } = this.state;
 
     switch (dialogField) {
-      case 'delegate':
-        return <VoteDelegate onSubmit={this.onDialogClose} />;
       case 'delegateRegistration':
         return <RegisterDelegate onSubmit={this.onDialogClose} />;
       case 'passphrase':
@@ -385,6 +383,10 @@ class AccountSettings extends React.Component<DecoratedProps, State> {
     return (
       <React.Fragment>
         <AccountNameDialog
+          account={this.account}
+          onNavigateBack={this.onDialogClose}
+        />
+        <VoteDelegateDialog
           account={this.account}
           onNavigateBack={this.onDialogClose}
         />

commit 30da9987fd7d6dd50b1cd0c49a215699376d5e1f

Fix delegate search in vote dialog violating underlying API constraints.
Regression introduced in 8aced0b. Closes #76.

src/containers/wallet/VoteDelegateDialog.tsx
@@ -60,7 +60,7 @@ class VoteDelegateDialog extends React.Component<Props, State> {
     async (query: string) => {
       const thisSearch = ++this.lastSearch;
       const { walletStore } = this.injected;
-      query = query.trim();
+      query = query.trim().toLowerCase();
 
       this.setState({
         search: {