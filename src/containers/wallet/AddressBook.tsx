beb8a68800eb0e0a8937ad02cd71f9eb2ae93fcb
Mart Roosmaa <mart@roosmaa.net>
2018-10-09 19:55:00 +0100

    Tweak address book UI for small screens. Fixes #102

--- src/containers/wallet/AddressBook.tsx
+++ src/containers/wallet/AddressBook.tsx
@@ -16,6 +16,7 @@ import TableCell from '@material-ui/core/TableCell';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import { inject, observer } from 'mobx-react';
 import * as React from 'react';
+import * as classNames from 'classnames';
 import { defineMessages, InjectedIntlProps, injectIntl } from 'react-intl';
 import AddressBookStore from '../../stores/addressBook';
 import { RouteLink } from '../../stores/root';
@@ -32,8 +33,11 @@ import RemoveContactDialog from './RemoveContactDialog';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 
-const styles = (theme: Theme) =>
-  createStyles({
+const styles = (theme: Theme) => {
+  const normalLayout = theme.breakpoints.up('sm');
+  const compactLayout = theme.breakpoints.down('xs');
+
+  return createStyles({
     content: {
       padding: theme.spacing.unit * 2
     },
@@ -43,12 +47,43 @@ const styles = (theme: Theme) =>
       bottom: 3 * theme.spacing.unit,
       zIndex: 1100
     },
-    contactRowAction: {
+    actionButton: {
       width: 28,
       height: 28,
       fontSize: theme.typography.pxToRem(14)
     },
+    cell: {
+      height: 'unset',
+      minHeight: 54,
+      padding: `${theme.spacing.unit}px ${2*theme.spacing.unit}px`,
+      '&:last-child': {
+        padding: `${theme.spacing.unit}px ${2*theme.spacing.unit}px`,
+      },
+      wordBreak: 'break-all',
+    },
+    compactName: {
+      marginTop: 4,
+    },
+    compactAddress: {
+      marginTop: 4,
+      marginBottom: 4,
+      fontStyle: 'italic',
+    },
+    actionsCell: {
+      textAlign: 'right',
+    },
+    onlyNormal: {
+      [compactLayout]: {
+        display: 'none',
+      },
+    },
+    onlyCompact: {
+      [normalLayout]: {
+        display: 'none',
+      },
+    },
   });
+};
 
 interface Props extends WithStyles<typeof styles> {}
 
@@ -65,6 +100,11 @@ const messages = defineMessages({
     id: 'wallet-address-book.new-contact-fab-tooltip',
     description: 'Tooltip for new contact floating action button',
     defaultMessage: 'New contact'
+  },
+  compactColumnHeader: {
+    id: 'wallet-address-book.compact-column-header',
+    description: 'Label for the compact column in address book',
+    defaultMessage: 'Contact details'
   },
   nameColumnHeader: {
     id: 'wallet-address-book.name-column-header',
@@ -126,23 +166,74 @@ class AddressBook extends React.Component<DecoratedProps> {
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell>
-                  {intl.formatMessage(messages.nameColumnHeader)}
-                </TableCell>
-                <TableCell>
-                  {intl.formatMessage(messages.addressColumnHeader)}
-                </TableCell>
-                <TableCell>
-                  {intl.formatMessage(messages.actionsColumnHeader)}
-                </TableCell>
+                <TableCell
+                  className={classNames(
+                    classes.cell,
+                    classes.onlyCompact
+                  )}
+                  children={intl.formatMessage(messages.compactColumnHeader)}
+                />
+                <TableCell
+                  className={classNames(
+                    classes.cell,
+                    classes.onlyNormal
+                  )}
+                  children={intl.formatMessage(messages.nameColumnHeader)}
+                />
+                <TableCell
+                  className={classNames(
+                    classes.cell,
+                    classes.onlyNormal
+                  )}
+                  children={intl.formatMessage(messages.addressColumnHeader)}
+                />
+                <TableCell
+                  className={classNames(
+                    classes.cell,
+                    classes.actionsCell
+                  )}
+                  children={intl.formatMessage(messages.actionsColumnHeader)}
+                />
               </TableRow>
             </TableHead>
             <TableBody>
               {addressBookStore.asArray.map(entry => (
                 <TableRow key={entry.id}>
-                  <TableCell>{entry.name}</TableCell>
-                  <TableCell children={entry.id} />
-                  <TableCell>
+                  <TableCell
+                    className={classNames(
+                      classes.cell,
+                      classes.onlyCompact
+                    )}
+                  >
+                    <div
+                      className={classes.compactName}
+                      children={entry.name}
+                    />
+                    <div
+                      className={classes.compactAddress}
+                      children={entry.id}
+                    />
+                  </TableCell>
+                  <TableCell
+                    className={classNames(
+                      classes.cell,
+                      classes.onlyNormal
+                    )}
+                    children={entry.name}
+                  />
+                  <TableCell
+                    className={classNames(
+                      classes.cell,
+                      classes.onlyNormal
+                    )}
+                    children={entry.id}
+                  />
+                  <TableCell
+                    className={classNames(
+                      classes.cell,
+                      classes.actionsCell
+                    )}
+                  >
                     <Tooltip
                       title={intl.formatMessage(messages.actionModifyTooltip)}
                     >
@@ -150,7 +241,7 @@ class AddressBook extends React.Component<DecoratedProps> {
                         route={addressBookModifyRoute}
                         params={{ id: entry.id }}
                       >
-                        <IconButton className={classes.contactRowAction}>
+                        <IconButton className={classes.actionButton}>
                           <EditIcon fontSize="inherit" />
                         </IconButton>
                       </Link>
@@ -162,7 +253,7 @@ class AddressBook extends React.Component<DecoratedProps> {
                         route={addressBookRemoveRoute}
                         params={{ id: entry.id }}
                       >
-                        <IconButton className={classes.contactRowAction}>
+                        <IconButton className={classes.actionButton}>
                           <DeleteIcon fontSize="inherit" />
                         </IconButton>
                       </Link>
