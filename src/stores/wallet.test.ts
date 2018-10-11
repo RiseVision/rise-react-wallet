e5a716e9245ce0a5af1d021b0783f51a7f7057f4
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2018-10-11 16:03:24 -0400

    Wallet store #74 - accounts

--- src/stores/wallet.test.ts
+++ src/stores/wallet.test.ts
@@ -1,12 +1,14 @@
 import { RouterStore } from 'mobx-router-rise';
 import { onboardingAddAccountRoute } from '../routes';
+import AccountStore from './account';
 import AddressBookStore from './addressBook';
 import TranslationsStore from './app';
 import { TConfig } from './index';
 import Wallet, { TStoredAccount } from './wallet';
 import * as sinon from 'sinon';
 import * as lstore from 'store';
-import { accounts as accountsFixture } from './fixtures';
+import { localAccounts, serverAccounts } from './fixtures';
+import { last } from 'lodash';
 
 const config: TConfig = {
   api_url: '',
@@ -21,8 +23,9 @@ let addressBook: AddressBookStore;
 let translations: TranslationsStore;
 
 beforeEach(() => {
-  // stub
+  // array to keep stubs to restore them later
   stubs = [];
+  // stub loadTranslation
   stubs.push(
     sinon.stub(TranslationsStore.prototype, 'loadTranslation').callsFake(() => {
       // empty
@@ -35,6 +38,7 @@ beforeEach(() => {
 });
 
 afterEach(() => {
+  lstore.clearAll();
   // check if a test has failed
   if (!stubs) return;
   // dispose all the stubs
@@ -53,21 +57,113 @@ describe('constructor', () => {
   });
   it('redirects when no account', () => {
     sinon.spy(router, 'goTo');
+    // init
     const wallet = new Wallet(config, router, addressBook, translations);
+    // @ts-ignore
     expect(router.goTo.calledWith(onboardingAddAccountRoute)).toBeTruthy();
   });
-  it.skip('signs in the latest account', () => {});
+  it('signs in the latest account', () => {
+    mockStoredAccounts(localAccounts);
+    lstore.set('lastSelectedAccount', localAccounts[1].id);
+    // init
+    const wallet = new Wallet(config, router, addressBook, translations);
+    expect(wallet.selectedAccount.id).toEqual(localAccounts[1].id);
+  });
+  it('signs in the first account when the latest is missing', () => {
+    mockStoredAccounts(localAccounts);
+    // init
+    const wallet = new Wallet(config, router, addressBook, translations);
+    expect(wallet.selectedAccount.id).toEqual(localAccounts[0].id);
+  });
 });
 
-describe('accounts', () => {
-  it.skip('saveAccount', () => {});
-  it.skip('getAccount', () => {});
-  it.skip('getAccountbyID', () => {});
-  it.skip('loadAccount', () => {});
-  it.skip('refreshAccount', () => {});
-  it.skip('login', () => {});
-  it.skip('observeAccount', () => {});
-  it.skip('selectAccount', () => {});
+describe.only('accounts', () => {
+  let wallet: Wallet;
+  beforeEach(() => {
+    mockStoredAccounts(localAccounts);
+    // TODO mock wallet.login, create server fixtures
+    wallet = new Wallet(config, router, addressBook, translations);
+  });
+  it('saveAccount', () => {
+    const id = localAccounts[0].id;
+    const name = 'test98fds7idfsh';
+    wallet.saveAccount({ id, name } as AccountStore);
+    const saved = last(Object.values(lstore.get('accounts'))) as TStoredAccount;
+    expect(saved.name).toEqual(name);
+  });
+  it('getAccountbyID', () => {
+    const id = localAccounts[0].id;
+    const account = wallet.getAccountByID(id);
+    expect(account.name).toEqual(localAccounts[0].name);
+  });
+  it('saveAccount (observable)', () => {
+    const id = localAccounts[0].id;
+    const name = 'test98fds7idfsh';
+    const account = wallet.getAccountByID(id);
+    account.name = name;
+    // observable auto saved the change
+    const saved = last(Object.values(lstore.get('accounts'))) as TStoredAccount;
+    expect(saved.name).toEqual(name);
+  });
+  it('loadAccount', async () => {
+    const id = localAccounts[0].id;
+    const balance = 983475;
+    // stub the API response
+    stubs.push(
+      // TODO extract
+      sinon.stub(wallet.dposAPI.accounts, 'getAccount').returns({
+        account: { address: id, balance },
+        success: true
+      })
+    );
+    // @ts-ignore protected
+    const response = await wallet.loadAccount(id);
+    expect(response.account).toEqual({ address: id, balance });
+  });
+  it('refreshAccount', async () => {
+    const id = localAccounts[0].id;
+    const balance = 983475;
+    // stub the API response
+    stubs.push(
+      // TODO extract
+      sinon.stub(wallet.dposAPI.accounts, 'getAccount').returns({
+        account: { address: id, balance },
+        success: true
+      })
+    );
+    await wallet.refreshAccount(id);
+    expect(wallet.accounts.get(id).balance.toNumber()).toEqual(balance);
+  });
+  it('login', async () => {
+    const id = localAccounts[0].id;
+    // stub the API response
+    stubs.push(
+      // TODO extract
+      sinon
+        .stub(wallet.dposAPI.accounts, 'getAccount')
+        .returns(serverAccounts[0])
+    );
+    // delete existing accounts
+    wallet.accounts.clear();
+    await wallet.login(id, localAccounts[0]);
+    const account = wallet.accounts.get(id);
+    expect(account.balance.toString()).toEqual(
+      serverAccounts[0].account.balance
+    );
+  });
+  it('selectAccount', () => {
+    const id = localAccounts[1].id
+    wallet.selectAccount(id)
+    expect(wallet.selectedAccount.id).toEqual(id)
+  });
+  it('idToName', () => {
+    const id = localAccounts[1].id
+    const name = localAccounts[1].name
+    // create a fake address book contact
+    wallet.addressBook.contacts.set('123R', 'test')
+    expect(wallet.idToName(id)).toEqual(name)
+    expect(wallet.idToName('123R')).toEqual('test')
+  });
   it.skip('getRecipientName', () => {});
   it.skip('registerAccount', () => {});
   it.skip('removeAccount', () => {});
@@ -87,4 +183,5 @@ describe('API', () => {
   it.skip('loadRegisteredDelegate', () => {});
   it.skip('loadRecentTransactions', () => {});
   it.skip('parseTransactionVotes', () => {});
+  it.skip('parseAccountReponse', () => {});
 });
