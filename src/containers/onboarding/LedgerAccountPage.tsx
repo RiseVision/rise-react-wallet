cc824093f8e6146bd398d99cb7acb27ed957e104
Mart Roosmaa <roosmaa@gmail.com>
2018-10-22 13:59:15 +0300

    Add confirm address step to Ledger account import

--- src/containers/onboarding/LedgerAccountPage.tsx
+++ src/containers/onboarding/LedgerAccountPage.tsx
@@ -17,11 +17,13 @@ import {
   injectIntl
 } from 'react-intl';
 import LedgerConnectIllustration from '../../components/LedgerConnectIllustration';
+import LedgerConfirmIllustration from '../../components/LedgerConfirmIllustration';
 import AccountIcon from '../../components/AccountIcon';
 import ModalPaper from '../../components/ModalPaper';
 import ModalPaperHeader from '../../components/ModalPaperHeader';
 import {
   onboardingAddAccountRoute,
+  accountOverviewRoute,
 } from '../../routes';
 import { AccountType } from '../../stores/account';
 import OnboardingStore from '../../stores/onboarding';
@@ -80,6 +82,20 @@ const messages = defineMessages({
     description: 'Label for the account number available for import',
     defaultMessage: 'Account #{number}',
   },
+  confirmInstructions: {
+    id: 'onboarding-ledger-account.confirm-instructions',
+    description: 'Text instructing the user to confirm their address on their Ledger',
+    defaultMessage: 'Please confirm the address above on your Ledger device.',
+  },
+  statusWaitingConfirmation: {
+    id: 'onboarding-ledger-account.status-waiting-confirmation',
+    description: 'Status text when waiting for user to confirm the account',
+    defaultMessage:
+      'Waiting for confirmation... ({seconds} {seconds, plural,' +
+      '  one {second}' +
+      '  other {seconds}' +
+      '} remaining)',
+  },
 });
 
 class AccountData {
@@ -109,7 +125,11 @@ class AccountData {
 class LedgerAccountPage extends React.Component<DecoratedProps> {
   private ledger: LedgerChannel;
   private disposeAccountLoader: null | IReactionDisposer = null;
+  private countdownId: null | number = null;
 
+  @observable private selectedAccount: null | AccountData = null;
+  @observable private selectionTimeout: null | Date = null;
+  @observable private countdownSeconds: number = 0;
   @observable private accounts = observable.array<AccountData>([]);
 
   get injected(): PropsInjected {
@@ -139,6 +159,7 @@ class LedgerAccountPage extends React.Component<DecoratedProps> {
   render() {
     const { intl, classes, ledgerStore } = this.injected;
     const { deviceId } = this.ledger;
+    const { selectedAccount, countdownSeconds } = this;
 
     return (
       <ModalPaper open={true}>
@@ -183,6 +204,52 @@ class LedgerAccountPage extends React.Component<DecoratedProps> {
               />
             </Grid>
           </Grid>
+        ) : selectedAccount !== null ? (
+          <React.Fragment>
+            <List>
+              <ListItem
+                key={selectedAccount.slot}
+                divider={true}
+              >
+                <ListItemAvatar>
+                  <Avatar className={classes.accountAvatar}>
+                    <AccountIcon size={24} address={selectedAccount.data ? selectedAccount.data.address : ''} />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={intl.formatMessage(
+                    messages.accountNrLabel,
+                    { number: selectedAccount.slot + 1 }
+                  )}
+                  secondary={selectedAccount.data ? selectedAccount.data.address : '...'}
+                />
+              </ListItem>
+            </List>
+            <Grid
+              container={true}
+              className={classes.content}
+              spacing={16}
+            >
+              <Grid item={true} xs={12}>
+                <Typography
+                  children={intl.formatMessage(messages.confirmInstructions)}
+                />
+              </Grid>
+              <Grid item={true} xs={12}>
+                <div className={classes.noPadding}>
+                  <LedgerConfirmIllustration />
+                </div>
+              </Grid>
+              <Grid item={true} xs={12}>
+                <Typography
+                  children={intl.formatMessage(
+                    messages.statusWaitingConfirmation,
+                    { seconds: countdownSeconds }
+                  )}
+                />
+              </Grid>
+            </Grid>
+          </React.Fragment>
         ) : (
           <List>
             {this.accounts.map((acc, idx) => (
@@ -212,20 +279,69 @@ class LedgerAccountPage extends React.Component<DecoratedProps> {
     );
   }
 
-  private async confirmImport(account: AccountData) {
-    // TODO: Switch to account importing UI
+  private updateSelectionCountdown = () => {
+    const now = new Date();
+    const remainMs = this.selectionTimeout !== null ? this.selectionTimeout.getTime() - now.getTime() : 0;
+    const isCountdownActive = remainMs > 0;
 
-    let success: boolean;
-    try {
-      success = await this.ledger.confirmAccount(account.slot);
-    } catch (ex) {
-      success = false;
+    runInAction(() => {
+      if (isCountdownActive) {
+        this.countdownSeconds = Math.ceil(remainMs / 1000);
+      } else {
+        // Make sure that the timeout clears the selected account
+        this.selectedAccount = null;
+      }
+    });
+
+    if (isCountdownActive && this.countdownId === null) {
+      this.countdownId = window.setInterval(this.updateSelectionCountdown, 250);
+    } else if (!isCountdownActive && this.countdownId !== null) {
+      window.clearInterval(this.countdownId);
+      this.countdownId = null;
     }
+  }
 
-    if (success) {
-      // TODO: Add account to store
-    } else {
-      // TODO: Switch back to account list
+  private async confirmImport(account: AccountData) {
+    const { walletStore, routerStore } = this.injected;
+    const { deviceId } = this.ledger;
+
+    if (account.data !== null) {
+      const { address: accountAddress } = account.data;
+
+      // Switch to account importing UI
+      runInAction(() => {
+        this.selectedAccount = account;
+        this.selectionTimeout = new Date(new Date().getTime() + 25000);
+        this.updateSelectionCountdown();
+      });
+
+      // Run the actual confirmation logic
+      let success: boolean;
+      try {
+        success = await this.ledger.confirmAccount(account.slot);
+      } catch (ex) {
+        success = false;
+      }
+
+      if (success && this.selectedAccount === account) {
+        // login(...) and goTo(...) need to be called outside of runInAction, to avoid
+        // weird order of execution and goTo(...) ending up being called before login(...)
+        walletStore.login(
+          accountAddress,
+          {
+            type: AccountType.LEDGER,
+            hwId: deviceId,
+            hwSlot: account.slot,
+          },
+          true
+        );
+        routerStore.goTo(accountOverviewRoute, { id: accountAddress });
+      } else {
+        runInAction(() => {
+          // Switch back to account list
+          this.selectedAccount = null;
+        });
+      }
     }
   }
 
@@ -234,6 +350,7 @@ class LedgerAccountPage extends React.Component<DecoratedProps> {
     const { walletStore } = this.injected;
     const { deviceId } = this.ledger;
 
+    this.selectedAccount = null;
     this.accounts = observable.array();
     if (deviceId === null) {
       return;
