f889da7dad6b58097c3190d4133b7f8a0e3ed3e8
Tobiasz Cudnik <tobiasz.cudnik@gmail.com>
2019-02-11 19:17:08 +0100

    reformat

--- src/containers/wallet/Wallet.tsx
+++ src/containers/wallet/Wallet.tsx
@@ -218,7 +218,7 @@ class Wallet extends React.Component<DecoratedProps, State> {
     this.setState({
       mobileDrawerOpen: !this.state.mobileDrawerOpen
     });
-  };
+  }
 
   handleConfirmSignOut = () => {
     const { walletStore, routerStore } = this.injected;
@@ -229,26 +229,26 @@ class Wallet extends React.Component<DecoratedProps, State> {
 
     walletStore.signout();
     routerStore.goTo(onboardingAddAccountRoute);
-  };
+  }
 
   handleCancelSignOutPrompt = () => {
     this.setState({
       signOutOpen: false
     });
-  };
+  }
 
   handleOpenSignOutPrompt = () => {
     this.setState({
       mobileDrawerOpen: false,
       signOutOpen: true
     });
-  };
+  }
 
   handleAfterNavigate = () => {
     this.setState({
       mobileDrawerOpen: false
     });
-  };
+  }
 }
 
 export default stylesDecorator(themeDecorator(Wallet));
